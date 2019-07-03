82a3360002d73082000aad6efc9394443e23777a
Andrii Shmyhovskyi <ansh.softevol@gmail.com>
2019-07-03 17:03:44 +0300

    workign version. revison 0.1

--- oop/hw2.py
+++ oop/hw2.py
@@ -2,11 +2,12 @@
 
 
 class Employee(object):
-    def __init__(self, first_name, second_name, salary, experiance):
+    def __init__(self, first_name, second_name, salary, experiance, higher_manager=None):
         self.first_name = first_name
         self.second_name = second_name
         self.salary = int(salary)
         self.experiance = int(experiance)
+        self.higher_manager = higher_manager
 
     def introduce(self):
         print("My name is " + self.first_name + " " + self.second_name)
@@ -19,20 +20,17 @@ class Employee(object):
         else:
             final_salary = self.salary
         return final_salary
-    
+#### need to check    
 #    def __repr__(self):
-# add manager
 #        return str(self.first_name) + " " + str(self.second_name) + ", manager: " + ", experiance: " + str(self.experiance)  
 
     def __str__(self):
-# add manager
-        return str(self.first_name) + " " + str(self.second_name) + ", manager: " + ", experiance: " + str(self.experiance)  
+        return str(self.first_name) + " " + str(self.second_name) + ", manager: " + str(self.higher_manager.first_name) + " " + str(self.higher_manager.second_name) + ", experiance: " + str(self.experiance)  
 
 
 class Developer(Employee):
     def __init__(self, first_name, second_name, salary, experiance, higher_manager=None):
-        super().__init__(first_name, second_name, salary, experiance)
-        self.higher_manager = higher_manager
+        super().__init__(first_name, second_name, salary, experiance, higher_manager)
 
     def show_higher_man(self):
         print("I am working with " + str(self.higher_manager))
@@ -40,9 +38,8 @@ class Developer(Employee):
 
 class Designer(Employee):
     def __init__(self, first_name, second_name, salary, experiance, eff_koef, higher_manager=None):
-        super().__init__(first_name, second_name, salary, experiance)
+        super().__init__(first_name, second_name, salary, experiance, higher_manager)
         self.eff_koef = float(eff_koef)
-        self.higher_manager = higher_manager
 
     def show_finance(self):
         final_salary = super().show_finance()
@@ -51,8 +48,8 @@ class Designer(Employee):
 
 class Manager(Employee):
     def __init__(self, first_name, second_name, salary, experiance, higher_manager=None, team_members=None):
-        super().__init__(first_name, second_name, salary, experiance)
-        self.higher_manager = higher_manager
+        super().__init__(first_name, second_name, salary, experiance, higher_manager)
+       # self.higher_manager = higher_manager
         if team_members is not None:
             self.team_members = team_members
         else:
@@ -90,45 +87,64 @@ class Department(object):
 
     def give_salary(self):
         for i in self.managers_list:
+            print(" --- " + i.second_name + "'s team ---")
             print(str(i.first_name) + " " + str(i.second_name) + " : got salary : " + str(i.show_finance()))
             for j in i.show_team_members():
                 print(str(j.first_name) + " " + str(j.second_name) + " : got salary : " + str(j.show_finance()))
 
+### end of code. add some data
+manager1 = Manager("Virktor", "Piatochnik1", 800, 3)
+manager2 = Manager("Virktor", "Piatochnik2", 700, 1)
+manager3 = Manager("Virktor", "Piatochnik3", 1000, 2, manager1)
 
+worker1 = Developer("Steve", "Wozniak1", 900 , 2, manager1) 
+worker2 = Developer("Steve", "Wozniak2", 1000 , 3) 
+worker3 = Developer("Steve", "Wozniak3", 1200 , 4) 
+worker4 = Developer("Steve", "Wozniak4", 1200 , 5) 
+worker5 = Developer("Steve", "Wozniak5", 1500 , 6) 
+worker6 = Developer("Steve", "Wozniak6", 1500 , 7) 
+worker7 = Developer("Steve", "Wozniak7", 1500 , 6) 
+worker8 = Developer("Steve", "Wozniak8", 1500 , 7) 
+worker9 = Developer("Steve", "Wozniak9", 1500 , 5) 
+worker10 = Developer("Steve", "Wozniak10", 2000 , 8) 
 
+designer1 = Designer("Dan", "Moris1", 1100, 5, 0.9)
+designer2 = Designer("Dan", "Moris2", 1200, 7, 0.7)
+designer3 = Designer("Dan", "Moris3", 1300, 3, 0.8)
+designer4 = Designer("Dan", "Moris4", 1400, 1, 0.6)
+designer5 = Designer("Dan", "Moris5", 1000, 6, 0.8)
+designer6 = Designer("Dan", "Moris6", 900, 2, 1.0, manager2)
 
-worker1 = Employee("Adriano", "Celentano", 900, 5)
-worker1.introduce()
-worker1.show_finance()
-worker2 = Developer("Jon", "Dou", 999, 5, "Jon Martin")
-worker2.show_higher_man()
-worker3 = Developer("Jon", "Dou2", 999, 6)
-worker3.show_higher_man()
-worker4 = Designer("Stive", "Jobs", 1000, 5, 1.0)
-worker5 = Designer("Steave", "Moris", 1100, 5, 0.9)
-manager1 = Manager("Tom", "Fozz", 888, 5)
-worker6 = Developer("Salaga", "Dou", 500, 1, "Jon Martin")
-
-worker4.show_finance()
 manager1.add_team_member(worker1)
 manager1.add_team_member(worker2)
-manager1.add_team_member(worker4)
-manager1.add_team_member(worker6)
-#manager1.show_team_members()
-#manager1.give_salary()
-manager2 = Manager("Tim", "Collins", 989, 6)
-#print("manager2-1")
-#manager2.show_team_members()
+#manager1.add_team_member(worker5)
+manager1.add_team_member(designer1)
+manager1.add_team_member(designer2)
+
 manager2.add_team_member(worker3)
+manager2.add_team_member(worker4)
 manager2.add_team_member(worker5)
-#print("manager2-2")
-#manager2.show_team_members()
-department1 = Department([manager1,manager2])
-department1.show_managers_list()
+manager2.add_team_member(worker6)
+manager2.add_team_member(designer3)
+manager2.add_team_member(designer4)
+manager2.add_team_member(designer5)
+manager2.add_team_member(designer6)
+
+manager3.add_team_member(worker7)
+manager3.add_team_member(worker8)
+manager3.add_team_member(worker9)
+manager3.add_team_member(worker10)
+manager3.add_team_member(worker6)
+manager3.add_team_member(designer1)
+manager3.add_team_member(designer2)
+manager3.add_team_member(designer3)
+manager3.add_team_member(designer4)
+manager3.add_team_member(designer5)
+manager3.add_team_member(designer6)
+
+department1 = Department([manager1,manager2,manager3])
 department1.give_salary()
-# print(manager1)
-print("print")
-#print(dir(worker2))
-print(worker2)
-print("self")
-worker2
+
+print(worker1)
+print(manager3)
+print(designer6)
